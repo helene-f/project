28c7c64974265debfa18782aff503fc3c051df41
helene <hfrignac@gmail.com>
2018-03-15 15:32:22 +0100

    template bootstrap clean blog added

---
+++ public/js/search-button.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#test").click(function () {
+        $("#demo").toggle();
+    });
+
+  /*  $(".signal").click(function () {
+        alert("etes-vous sur de vouloir signaler ce commentaire auprès de l'administrateur du site ?");
+    });*/
+
+});
