2b150292970de0fd545dc1f96975fd59cc9d5596
helene <hfrignac@gmail.com>
2018-03-15 17:05:28 +0100

    added comments about autoload and twig

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <?php
-require('controler/frontOffice.php');
-require('controler/backOffice.php');
+/*define('BASE_PATH', dirname(realpath(__FILE__)) . '/');
+define('VIEW_PATH', BASE_PATH . 'views/');
+// Twig
+require_once BASE_PATH.'vendors/twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem(VIEW_PATH);
+$twig = new Twig_Environment($loader, array(
+				'cache'	=>	BASE_PATH.'cache',
+				'debug'	=>	true,
+		  'auto_reload'	=>	true,
+	 'strict_variables'	=>	true,
+		   'autoescape'	=>	true,
+
+));
+// Use an autoloader...
+require BASE_PATH . 'libs/Bootstrap.php';
+require BASE_PATH . 'libs/Controller.php';
+require BASE_PATH . 'libs/Model.php';
+require BASE_PATH . 'libs/View.php';
+new Bootstrap();
+$view = new View();
+$view->render(
+		$twig->display('base_template.twig')
+	);*/
+
+
+
+require('controller/frontOffice.php');
+/*require('controler/backOffice.php');*/
 
 try {
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'listPosts') {
         listPosts();
     }
+
     elseif ($_GET['action'] == 'post') {
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             post();
