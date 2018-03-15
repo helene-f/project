b0632367c5afd25dfd896000a1381125094f8501
helene <hfrignac@gmail.com>
2018-03-15 17:03:49 +0100

    added error page

--- view/frontOffice/error.php
+++ view/frontOffice/error.php
@@ -1,1 +1,19 @@
-<h2>Page non trouvée</h2>
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Erreur 404</title>
+
+
+    <link href="/public/css/style-error.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <h1>Error 404</h1>
+    <p>La page que vous recherchez ne peut être trouvée.</p>
+  </body>
+</html>
