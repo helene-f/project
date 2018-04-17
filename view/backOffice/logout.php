7d9d3553ca0c9ab53145d4910cca0a6fb1e4240d
helene <hfrignac@gmail.com>
2018-04-17 10:34:28 +0200

    added file to logout from the backOffice

---
+++ view/backOffice/logout.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Jean Forteroche vous livre son récit d'aventures lorsqu'il a fait le choix de partir vivre en Alaska.">
+    <meta name="author" content="Jean Forteroche">
+
+    <title>Page de déconnexion</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/public/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="/public/font-awesome/css/font-awesome.min.css" rel="stylesheet" ty
+    pe="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="/public/css/clean-blog.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="deconnexion">
+      <h1>Déconnexion</h1>
+      <p>Etes-vous sur de vouloir vous déconnecter ?</p>
+	  <button type="button" name="logout"><a href="/index.php?action=logout">Oui, je me déconnecte</a></button>
+	  <button type="button" name="stayLogin"><a href="/view/backOffice/listPostsEdit.php">Non, je reste sur l'espace d'administration.</a></button>
+    </div>
+
+  </body>
+</html>
