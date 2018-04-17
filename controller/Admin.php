6a1be26355ebfd45a88914e4e525a9742cf50607
helene <hfrignac@gmail.com>
2018-04-17 11:01:42 +0200

    added function register to create some admin accounts

--- controller/Admin.php
+++ controller/Admin.php
@@ -2,77 +2,87 @@
 //use \OpenClassrooms\Blog\Model\ConnexionManager;
 require_once('model/ConnexionManager.php');
 
-	function register($adminName, $adminEmail, $password)
-	{
+function register($adminName, $adminEmail, $psw_hache)
+{
+	$adminName = htmlspecialchars($_POST['adminName']);
+	$adminEmail = htmlspecialchars($_POST['email']);
+	$password = $_POST['password'];
+	$password2 = $_POST['password2'];
+	$psw_hache = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+	if ($password == $password2) {
 		$connexionManager = new ConnexionManager();
+		$addedAdmin = $connexionManager->addAdmin($adminName, $adminEmail, $psw_hache);
 
-		$addedAdmin = $contactManager->addAdmin($adminName, $adminEmail, $password);
+	if ($addedAdmin === false) {
+		throw new Exception ('Impossible d\'ajouter un autre administrateur !');
+	}
+	else {
+		echo ("vous êtes désormais administrateur du site");
+	}
+}
+	else {
+		echo "les mots de passe sont différents. Réessayez !";
+	}
+}
 
-		if ($addedAdmin === false) {
-			throw new Exception ('Impossible d\'ajouter un autre administrateur !');
+
+function login()
+{
+
+	$connexionManager = new ConnexionManager();
+	$logResult = $connexionManager->log();
+
+
+	//We try to compare password sent accross the form with the database password
+	$isPasswordCorrect = password_verify($_POST['password'], $logResult['password']);
+
+	if (!$logResult)
+	{
+		echo ('Mauvais identifiant ou mot de passe. Réessayez !');
+	}
+	else {
+		if ($isPasswordCorrect) {
+			session_start();
+			$_SESSION['id'] = $logResult['id'];
+			$_SESSION['admin'] = $_POST['adminName'];
+			//echo 'Bonjour '.htmlspecialchars($_SESSION['admin']).', vous êtes connecté';
+			header("Location:/view/backOffice/listPostsEdit.php");
 		}
 		else {
-			echo ("vous êtes désormais administrateur du site");
+			echo ('Mauvais identifiant ou mot de passe !');
+			echo password_hash("test", PASSWORD_DEFAULT);
+			var_dump($logResult);
+
+			var_dump($isPasswordCorrect);
+
+			var_dump($_POST['password']);
+			var_dump($logResult['password']);
+
+			var_dump($_SESSION['id']);
+			var_dump($_SESSION['admin']);
+			var_dump($_POST['adminName']);
+			var_dump($logResult['id']);
+
 		}
 	}
 
+}
 
-	function login()
-	{
+function logout()
+{
+	// does logout procedures
+	session_start();
 
-		$connexionManager = new ConnexionManager();
-		$logResult = $connexionManager->log();
-
-
-		//We try to compare password sent accross the form with the database password
-		$isPasswordCorrect = password_verify($_POST['password'], $logResult['password']);
-
-		if (!$logResult)
-		{
-			echo ('Mauvais identifiant ou mot de passe. Réessayez !');
-		}
-		else {
-			if ($isPasswordCorrect) {
-				session_start();
-				$_SESSION['id'] = $logResult['id'];
-				$_SESSION['admin'] = $_POST['adminName'];
-				echo 'Bonjour '.htmlspecialchars($_SESSION['admin']).', vous êtes connecté';
-				header("Location:/view/backOffice/listPostsEdit.php");
-			}
-			else {
-				echo ('Mauvais identifiant ou mot de passe !');
-				echo password_hash("test", PASSWORD_DEFAULT);
-				var_dump($logResult);
-
-				var_dump($isPasswordCorrect);
-
-				var_dump($_POST['password']);
-				var_dump($logResult['password']);
-
-				var_dump($_SESSION['id']);
-				var_dump($_SESSION['admin']);
-				var_dump($_POST['adminName']);
-				var_dump($logResult['id']);
-
-			}
-		}
-
-	}
-
-	function logout()
-	{
-		// does logout procedures
-		session_start();
-
-		// Delete session
-		$_SESSION = array();
-		session_destroy();
+	// Delete session
+	$_SESSION = array();
+	session_destroy();
 
 
-		// Delete cookies from the automatic connexion
-		setcookie('login', '');
-		setcookie('pass_hache', '');
+	// Delete cookies from the automatic connexion
+	setcookie('login', '');
+	setcookie('psw_hache', '');
 
-		header('Location: index.php');
-		exit();
-	}
+	header("Location:/index.php");
+	//exit();
+}
--- index.php
+++ index.php
@@ -1,68 +1,133 @@
 <?php
-/*define('BASE_PATH', dirname(realpath(__FILE__)) . '/');
-define('VIEW_PATH', BASE_PATH . 'views/');
-// Twig
-require_once BASE_PATH.'vendors/twig/lib/Twig/Autoloader.php';
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem(VIEW_PATH);
-$twig = new Twig_Environment($loader, array(
-				'cache'	=>	BASE_PATH.'cache',
-				'debug'	=>	true,
-		  'auto_reload'	=>	true,
-	 'strict_variables'	=>	true,
-		   'autoescape'	=>	true,
-
-));
-// Use an autoloader...
-require BASE_PATH . 'libs/Bootstrap.php';
-require BASE_PATH . 'libs/Controller.php';
-require BASE_PATH . 'libs/Model.php';
-require BASE_PATH . 'libs/View.php';
-new Bootstrap();
-$view = new View();
-$view->render(
-		$twig->display('base_template.twig')
-	);*/
-
-
-
 require('controller/frontOffice.php');
-/*require('controler/backOffice.php');*/
+//require('controller/backOffice.php');
+require('controller/Admin.php');
 
 try {
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'listPosts') {
-        listPosts();
-    }
+	if (isset($_GET['action'])) {
 
-    elseif ($_GET['action'] == 'post') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            post();
-        }
-        else {
-          // Erreur ! On arrête tout, on envoie un exception, donc on saute directement au catch
-            throw new Exception ('Aucun identifiant de billet envoyé');
-        }
-    }
-  elseif ($_GET['action'] == 'addComment') {
-      if (isset($_GET['id']) && $_GET['id'] > 0) {
-          if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-              addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-          }
-          else {
-              throw new Exception ('tous les champs ne sont pas remplis !');
-          }
-      }
-      else {
-          throw new Exception ('aucun identifiant de billet envoyé');
-      }
-  }
+		/* ----------------
+		FRONT OFFICE
+		-------------------  */
+
+		if ($_GET['action'] == 'listPosts') {
+			listPosts();
+		}
+
+		elseif ($_GET['action'] == 'post') {
+			if (isset($_GET['id']) && $_GET['id'] > 0) {
+				post();
+			}
+			else {
+				// Erreur ! On arrête tout, on envoie un exception, donc on saute directement au catch
+				throw new Exception ('Aucun identifiant de billet envoyé');
+			}
+		}
+
+		elseif ($_GET['action'] == 'addComment') {
+			if (isset($_GET['id']) && $_GET['id'] > 0) {
+				if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+					addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+				}
+				else {
+					throw new Exception ('tous les champs ne sont pas remplis !');
+				}
+			}
+			else {
+				throw new Exception ('aucun identifiant de billet envoyé');
+			}
+		}
+
+		elseif ($_GET['action'] == 'addContact') {
+			//if (isset($_GET['contactId']) && $_GET['contactId'] > 0) {
+			if (!empty($_POST['contactName']) && !empty($_POST['contactEmail'])) {
+				addContact($_POST['contactName'], $_POST['contactEmail']);
+			}
+			else {
+				throw new Exception ('tous les champs ne sont pas remplis !');
+			}
+		}
+
+		elseif ($_GET['action'] == 'register') {
+			if (!empty($_POST['adminName']) AND !empty($_POST['email']) AND !empty($_POST['password']) AND !empty($_POST['password2'])) {
+				register($_POST['adminName'], $_POST['email'], $_POST['password']);
+			}
+			else {
+				throw new Exception ('tous les champs ne sont pas remplis !');
+			}
+		}
+		elseif ($_GET['action'] == 'login') {
+			if (!empty($_POST['adminName']) && !empty($_POST['password'])) {
+				login();
+				if (isset($POST['AutomaticConnexion'])) {
+					echo "connexion automatique";
+					setcookie('login', ($_POST['adminName']), time()+3600);
+					setcookie('psw_hache', ($_POST['password']), time()+3600);
+				}
+				else {
+					echo 'cela ne marche pas, pas de possibilité de créer des cookies';
+				}
+			}
+			else {
+				throw new Exception ('tous les champs ne sont pas remplis !');
+			}
+		}
+
+		elseif ($_GET['action'] == 'logout') {
+				logout();
+			}
+
+		/* ----------------
+		BACK OFFICE
+		-------------------  */
+
+		/*
+		elseif ($_GET['action'] == 'listPostsAdmin') {
+		listPostsAdmin();
+	}
+
+	elseif ($_GET['action'] == 'postAdmin') {
+	if (isset($_GET['id']) && $_GET['id'] > 0) {
+	postAdmin();
 }
 else {
-  listPosts();
+throw new Exception('Aucun identifiant de chapitre');
+}
+}
+
+elseif ($_GET['action'] == 'addPost') {
+if (!empty($_POST['chapterTitle']) && !empty($_POST['chapterContent'])) {
+addPost($_POST['chapterTitle'], $_POST['chapterContent']);
+}
+else {
+throw new Exception ('tous les champs ne sont pas remplis !');
+}
+}
+
+
+
+
+
+/*
+elseif ($_GET['action'] == 'searchNav') {
+if(isset($_POST['q']) && !empty($_POST['q'])) {
+searchNav($_POST['q']);
+}
+else {
+// Erreur ! On arrête tout, on envoie un exception, donc on saute directement au catch
+throw new Exception ('Aucune recherche n\'a été effectuée');
+}
+}
+
+*/
+
+
+}
+else {
+	listPosts();
 }
 }
 catch(Exception $e) {
-  $errorMessage = $e->getMessage();
-  require('view/frontOffice/error.php');
+	$errorMessage = $e->getMessage();
+	require('view/frontOffice/error.php');
 }
