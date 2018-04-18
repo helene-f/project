85a8e05153272307fae74ef38e3b904d2658544c
helene <hfrignac@gmail.com>
2018-04-18 21:48:31 +0200

    added BS js lines

--- view/frontOffice/error.php
+++ view/frontOffice/error.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Jean Forteroche vous livre son récit d'aventures lorsqu'il a fait le choix de partir vivre en Alaska.">
+    <meta name="author" content="Jean Forteroche">
 
-    <title>Erreur 404</title>
+    <title>Erreur</title>
 
+    <!-- Bootstrap core CSS -->
+    <link href="public/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="/public/css/style-error.css" rel="stylesheet">
+    <!-- Custom fonts for this template -->
+    <link href="public/font-awesome/css/font-awesome.min.css" rel="stylesheet" ty
+    pe="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="public/css/clean-blog.min.css" rel="stylesheet">
+    <link href="/public/css/landing-style.css" rel="stylesheet">
 
   </head>
 
   <body>
-    <h1>Error 404</h1>
-    <p>La page que vous recherchez ne peut être trouvée.</p>
+
+    <div class="error-message">
+      <h1>Error 404</h1>
+      <p>La page que vous recherchez ne peut être trouvée.</p>
+      <p><a href="/index.php">Retournez à la page d'accueil</p>
+    </div>
+	<!-- Bootstrap core JavaScript -->
+	<script src="/public/js/jquery/jquery.min.js"></script>
+	<script src="/public/js/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Custom scripts for this template -->
+	<script src="/public/js/clean-blog.min.js"></script>
+
+
   </body>
 </html>
