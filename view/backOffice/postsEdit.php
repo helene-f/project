67d5ded27f37fd610aac554570fdbd04c06be7c8
helene <hfrignac@gmail.com>
2018-04-19 10:08:09 +0200

    added lines of php to choice between 2 actions and 2 values depanding of the links to create or modify a post

--- view/backOffice/postsEdit.php
+++ view/backOffice/postsEdit.php
@@ -1,44 +1,61 @@
-<html>
-  <head>
-    <title>Editer un chapitre</title>
-    <meta charset="utf-8">
+<?php include ('header.php');
 
-  </head>
-  <body>
+$action = '';
+if(isset($_GET['id']) && $_GET['id'] > 0) {
+  $action = '/admin.php?action=modifyPost&amp;id='.$post['id'];
+  $value = 'Modifier';
+} else {
+  $action = '/admin.php?action=addPost';
+  $value = 'Ajouter';
+}
+
+var_dump($newTitle);
+var_dump($newContent);
+var_dump($_POST['chapterTitle']);
+var_dump($_POST['chapterContent']);
+var_dump($post['title']);
+?>
+
+
     <h2>Editer un chapitre</h2>
-    <br /><br />
-    <form class="edit" action="index.php?action=addContent&amp;id=<?= $post['id'] ?>" method="post">
-      <label for="url">Lien de votre image : </label>
-      <input id="url" type="text" placeholder="votre url" name="url" value="<?php if(isset($admin)) {echo $admin;} ?>" />
-      <input type="submit" value="Ajouter" name="formEdit"></input>
-      <br />
-      <label for="title">Titre du chapitre : </label>
-      <input id="title" type="text" placeholder="Titre du chapitre" name="title" value="<?php if(isset($admin)) {echo $admin;} ?>" />
-      <br />
-      <label for="texte">Votre article : </label>
-      <textarea class="tinymce" id="article" type="text" placeholder="votre article" name="article"></textarea>
-      <br />
-      <input type="submit" value="Publier" name="formEdit"></input>
+
+    <div class="container">
+    <form class="addPost" action="<?php echo $action; ?>" method="post">
+      <!--div class="form-group">
+          <label for="picture">Téléchargez votre image : </label>
+          <input id="picture" type="file" placeholder=" Votre image" name="picture"/>
+      </div-->
+
+      <div class="form-group">
+          <label for="title">Titre du chapitre : </label>
+          <input id="title" type="text" placeholder=" Titre du chapitre" name="chapterTitle" value="<?php if(isset($post['title'])) {echo htmlspecialchars($post['title']);} ?>"/>
+      </div>
+
+      <div class="form-group">
+          <label for="article">Votre article : </label>
+          <textarea class="tinymce" id="article" type="text" placeholder="votre article" name="chapterContent"><?php if(isset($post['content'])) {echo htmlspecialchars($post['content']);} ?></textarea>
+      </div>
+
+	<input type="submit" class="btn btn-primary" value="<?php echo $value; ?>"/>
+
     </form>
-        <?php
-        if(isset($error))
-        {
-          echo $error;
-        }
-         ?>
-         <script src="../../public/js/plugin/tinymce/tinymce.min.js"></script>
-       <script>tinymce.init({ selector:'textarea', height: 500,
-  theme: 'modern',
-  plugins: 'preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
-  toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
-  image_advtab: true,
-  templates: [
-    { title: 'Test template 1', content: 'Test 1' },
-    { title: 'Test template 2', content: 'Test 2' }
+</div>
+
+         <script src="/public/js/plugin/tinymce/tinymce.min.js"></script>
+		<script> tinymce.init({
+  selector: 'textarea.tinymce',
+  height: 500,
+  menubar: false,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor textcolor',
+    'searchreplace visualblocks fullscreen',
+    'insertdatetime media table contextmenu paste help wordcount'
   ],
+  toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
   content_css: [
-    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-  ]
- });</script>
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i'],
+	cleanup: true
+});
+       </script>
   </body>
 </html>
