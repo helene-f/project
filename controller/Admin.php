f4ec51a1b016ff8c3a4c834f4bb6581440323284
helene <hfrignac@gmail.com>
2018-04-19 16:46:12 +0200

    debug view of ListPostsEdit.php thanks to a new header location with an url & action

--- controller/Admin.php
+++ controller/Admin.php
@@ -14,13 +14,13 @@ function register($adminName, $adminEmail, $psw_hache)
 		$connexionManager = new ConnexionManager();
 		$addedAdmin = $connexionManager->addAdmin($adminName, $adminEmail, $psw_hache);
 
-	if ($addedAdmin === false) {
-		throw new Exception ('Impossible d\'ajouter un autre administrateur !');
+		if ($addedAdmin === false) {
+			throw new Exception ('Impossible d\'ajouter un autre administrateur !');
+		}
+		else {
+			echo ("vous êtes désormais administrateur du site");
+		}
 	}
-	else {
-		echo ("vous êtes désormais administrateur du site");
-	}
-}
 	else {
 		echo "les mots de passe sont différents. Réessayez !";
 	}
@@ -29,10 +29,8 @@ function register($adminName, $adminEmail, $psw_hache)
 
 function login()
 {
-
 	$connexionManager = new ConnexionManager();
 	$logResult = $connexionManager->log();
-
 
 	//We try to compare password sent accross the form with the database password
 	$isPasswordCorrect = password_verify($_POST['password'], $logResult['password']);
@@ -46,28 +44,17 @@ function login()
 			session_start();
 			$_SESSION['id'] = $logResult['id'];
 			$_SESSION['admin'] = $_POST['adminName'];
+
 			//echo 'Bonjour '.htmlspecialchars($_SESSION['admin']).', vous êtes connecté';
-			header("Location:/view/backOffice/listPostsEdit.php");
+			//header("Location:/view/backOffice/listPostsEdit.php");
+			header("Location:/admin.php?action=getForTable");
 		}
 		else {
 			echo ('Mauvais identifiant ou mot de passe !');
-			echo password_hash("test", PASSWORD_DEFAULT);
-			var_dump($logResult);
-
-			var_dump($isPasswordCorrect);
-
-			var_dump($_POST['password']);
-			var_dump($logResult['password']);
-
-			var_dump($_SESSION['id']);
-			var_dump($_SESSION['admin']);
-			var_dump($_POST['adminName']);
-			var_dump($logResult['id']);
-
 		}
 	}
-
 }
+
 
 function logout()
 {
@@ -78,11 +65,9 @@ function logout()
 	$_SESSION = array();
 	session_destroy();
 
-
 	// Delete cookies from the automatic connexion
 	setcookie('login', '');
 	setcookie('psw_hache', '');
 
 	header("Location:/index.php");
-	//exit();
 }
