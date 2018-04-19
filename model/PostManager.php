fc784958cc5769015698cf4bb70a1c9563af0e05
helene <hfrignac@gmail.com>
2018-04-19 10:01:12 +0200

    added and changed management of posts : add, count, edit and update and delete

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,39 +1,89 @@
 <?php
+
+//namespace Openclassrooms\Blog\Model;
+
 require_once("model/Manager.php");
 
 class PostManager extends Manager
 {
-  public function getPosts()
+	/* -----
+		CRUD
+	   -----
+	*/
+
+	public function createPost($title, $content)
+	{
+		$db = $this->dbConnect();
+		$posts = $db->prepare('INSERT INTO posts(title, content, creation_date) VALUES(?, ?, NOW())');
+		$affectedLines = $posts->execute(array($title, $content));
+
+		return $affectedLines;
+	}
+
+	/*public function count()
   {
-      $db = $this->dbConnect();
-      $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr, picture FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+	  	$db = $this->dbConnect();
+		$req = $db->query('SELECT COUNT(*) FROM posts');
+		$count = $req->fetchColumn();
 
-      return $req;
-  }
-
-  public function getPost($postId)
-  {
-      $db = $this->dbConnect();
-      $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr, picture FROM posts WHERE id = ?');
-      $req->execute(array($postId));
-      $post = $req->fetch();
-
-      return $post;
-  }
-
-  /*public function editPost($id, $title, $content, $author, $picture)
-  {
-      $db = $this->dbConnect();
-      $posts = $db->prepare('INSERT INTO posts (id, title, content, author, creation_date, picture) VALUES(?, ?, ?, ?, NOW(), ?)');
-      $affectedLines = $posts->execute(array($id, $picture, $title, $content, $author, $picture));
-
-      return $affectedLines;
-  }
-
-  public function deletePost($postId)
-  {
-      $id  = $_GET['postId'];
-      $db = $this->dbConnect();
-      $post = $db->execute('DELETE FROM posts WHERE postId = '.$id'');
+		return $count;
   }*/
-}
+
+
+	public function getPosts()
+	{
+		$db = $this->dbConnect();
+		$req = $db->query("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS creation_date_fr, picture FROM posts ORDER BY id DESC LIMIT 0, 5");
+
+		return $req;
+	}
+
+	public function getPostsList()
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('SELECT * FROM posts');
+		$req->execute();
+
+		return $req;
+	}
+
+	public function getPost($postId)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS creation_date_fr, picture FROM posts WHERE id = ?");
+		$req->execute(array($postId));
+		$post = $req->fetch();
+
+		return $post;
+	}
+
+	public function updatePost($newTitle, $newContent, $postId)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('UPDATE posts SET title = ?, content = ? WHERE id = ?');
+		$affectedLines = $req->execute(array($newTitle, $newContent, $postId));
+
+		return $affectedLines;
+	}
+
+	public function deletePost($postId)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare("DELETE p, c FROM posts p INNER JOIN comments c ON c.post_id = p.id WHERE p.id= ?");
+		$req->execute(array($postId));
+
+		return $req;
+
+
+	}
+
+		/*
+		public function searchWord($q)
+		{
+		$db = $this->dbConnect();
+		$req = $db->prepare('SELECT id, title, content FROM posts WHERE title LIKE ? OR content LIKE ?');
+		$req->execute(array('%'.$q.'%', '%'.$q.'%'));
+
+		return $req;
+		}*/
+		}
