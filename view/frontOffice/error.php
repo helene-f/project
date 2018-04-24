7c89208e334c7e8f7ee3582821ccd2a3e100c9b1
helene <hfrignac@gmail.com>
2018-04-24 15:34:43 +0200

    added errorMessage to the error file to a better view of errors

--- view/frontOffice/error.php
+++ view/frontOffice/error.php
@@ -2,42 +2,45 @@
 <html lang="fr">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Jean Forteroche vous livre son récit d'aventures lorsqu'il a fait le choix de partir vivre en Alaska.">
-    <meta name="author" content="Jean Forteroche">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Jean Forteroche vous livre son récit d'aventures lorsqu'il a fait le choix de partir vivre en Alaska.">
+	<meta name="author" content="Jean Forteroche">
 
-    <title>Erreur</title>
+	<title>Erreur</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="public/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap core CSS -->
+	<link href="public/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template -->
-    <link href="public/font-awesome/css/font-awesome.min.css" rel="stylesheet" ty
-    pe="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<!-- Custom fonts for this template -->
+	<link href="public/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
-    <!-- Custom styles for this template -->
-    <link href="public/css/clean-blog.min.css" rel="stylesheet">
-    <link href="/public/css/landing-style.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="public/css/clean-blog.min.css" rel="stylesheet">
+	<link href="/public/css/login-style.css" rel="stylesheet">
 
-  </head>
+</head>
+<body>
+	<div class="container">
+		<div class="card card-container">
+			<h2 class="card-header">Erreur</h2>
+			<div class="card-body">
+				<p>L'erreur suivante a été trouvée :</p>
+				<?= $errorMessage ?>
+				<p><a href="/index.php">Retournez à la page d'accueil</p>
+				</div>
+			</div>
+		</div>
 
-  <body>
+		<!-- Bootstrap core JavaScript -->
+		<script src="/public/js/jquery/jquery.min.js"></script>
+		<script src="/public/js/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <div class="error-message">
-      <h1>Error 404</h1>
-      <p>La page que vous recherchez ne peut être trouvée.</p>
-      <p><a href="/index.php">Retournez à la page d'accueil</p>
-    </div>
-	<!-- Bootstrap core JavaScript -->
-	<script src="/public/js/jquery/jquery.min.js"></script>
-	<script src="/public/js/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-	<!-- Custom scripts for this template -->
-	<script src="/public/js/clean-blog.min.js"></script>
+		<!-- Custom scripts for this template -->
+		<script src="/public/js/clean-blog.min.js"></script>
 
 
-  </body>
-</html>
+	</body>
+	</html>
